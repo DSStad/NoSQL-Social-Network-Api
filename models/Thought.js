71100fb425a15fc3799969fdb3d708f62a583f51
Derick Stadler <derickstadler@gmail.com>
2023-07-17 23:19:34 -0600

    started to create Thought model

---
+++ models/Thought.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // get: v => ``
+        },
+        username: {
+            type: String,
+            required: true,
+        }
+    }
+)
